5a4074a583d687b136eb64ef25f031358502a711
Troy Jendra <troy@troyjendra.com>
2023-01-06 16:14:25 +1100

    Move and symlink config files into etc to enable mounting etc in a container volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # execute in path where github actions runner was untarred
 cd /usr/local
 
-if [ ! -f .runner ]; then
+if [ ! -f etc/.runner ]; then
   echo "Configuring the github actions runner"
   ./config.sh \
     --disableupdate \
@@ -14,7 +14,15 @@ if [ ! -f .runner ]; then
     --unattended \
     --url "https://github.com/${GITHUB_ACTIONS_RUNNER_GITHUB_ORG}" \
     --work /usr/local/_work/
+
+  # move config files into the etc directory, enabling mounting etc in the container volume
+  mv .credentials .credentials_rsaparams .runner etc/
 fi
+
+# symlink config files from the mounted container volume in etc
+ln --force --symbolic etc/.credentials .credentials
+ln --force --symbolic etc/.credentials_rsaparams .credentials_rsaparams
+ln --force --symbolic etc/.runner .runner
 
 echo "Starting the github actions runner"
 ./run.sh
